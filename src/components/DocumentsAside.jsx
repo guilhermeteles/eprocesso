3495f1b917e94215bc27e2b032aa4c064b64220a
guilhermeteles@gmail.com <guilhermeteles@gmail.com>
2024-10-23 08:52:47 -0300

    toggle

--- src/components/DocumentsAside.jsx
+++ src/components/DocumentsAside.jsx
@@ -139,7 +139,7 @@ export const DocumentsAside = () => {
             onChange={toggleViewMode}
             className="sr-only peer"
           />
-          <div className="w-10 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-500"></div>
+          <div className="w-10 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-500"></div>
         </div>
 
         <FontAwesomeIcon 
