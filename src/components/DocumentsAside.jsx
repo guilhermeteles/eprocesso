c94432f59d4d5164fdf248e87dfd9a89d38f4fe6
guilhermeteles@gmail.com <guilhermeteles@gmail.com>
2024-10-23 10:33:01 -0300

    fix toggle

--- src/components/DocumentsAside.jsx
+++ src/components/DocumentsAside.jsx
@@ -138,8 +138,9 @@ export const DocumentsAside = () => {
             checked={viewMode === 'chronological'}
             onChange={toggleViewMode}
             className="sr-only peer"
+            
           />
-          <div className="w-10 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-500"></div>
+          <div onClick={toggleViewMode}  className="w-10 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-500"></div>
         </div>
 
         <FontAwesomeIcon 
